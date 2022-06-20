34f613e9868a3ca4cf329bdf74e5ce3e3615e99e
pcranaway <pcranaway@tuta.io>
2022-06-21 00:37:23 +0200

    fixed potential future error

--- src/main/kotlin/protocol/packet/PacketRegistry.kt
+++ src/main/kotlin/protocol/packet/PacketRegistry.kt
@@ -3,7 +3,7 @@ package protocol.packet
 import protocol.packet.impl.handshake.HandshakePacket
 
 object PacketRegistry {
-    val packetMap = mapOf(
+    val packetMap: Map<Int, () -> Packet> = mapOf(
         0x00 to {
             HandshakePacket()
         }
