1c0cd7ebd280257f9f8fde9638b77a278459a228
pcranaway <pcranaway@tuta.io>
2022-06-21 15:38:11 +0200

    fix registry

--- src/main/kotlin/protocol/packet/PacketRegistry.kt
+++ src/main/kotlin/protocol/packet/PacketRegistry.kt
@@ -5,12 +5,19 @@ import protocol.packet.impl.status.RequestPacket
 import server.connection.State
 
 object PacketRegistry {
-    val packets = mapOf(
-        PacketId(0, State.Handshake, Sender.Client) to { HandshakePacket() },
-        PacketId(0, State.Status, Sender.Client) to { RequestPacket() },
-    )
+
+    val packets: Map<Sender, Map<State, Map<Int, () -> Packet>>> = mapOf(
+        Sender.Client to mapOf(
+            State.Handshake to mapOf(
+                0 to { HandshakePacket() },
+            ),
+            State.Status to mapOf(
+                0 to { RequestPacket() },
+            ),
+        ))
 
     fun findPacket(id: Int, state: State, sender: Sender): (() -> Packet)? {
-        return packets[PacketId(id, state, sender)]
+        return packets[sender]?.get(state)?.get(id)
     }
+
 }
