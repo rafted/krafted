4b66e11685341ce9e5059d1870fe4f04d12bc0f5
patrickzondervan <rawrdev@protonmail.com>
2022-06-22 03:44:02 +0200

    basic ChatComponent system

--- src/main/kotlin/util/serializers.kt
+++ src/main/kotlin/util/serializers.kt
@@ -1,10 +1,13 @@
 package util
 
+import chat.COLOR_ID_MAP
+import chat.ChatColor
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.awt.Color
 import java.util.*
 
 object UUIDSerializer : KSerializer<UUID> {
@@ -16,5 +19,27 @@ object UUIDSerializer : KSerializer<UUID> {
 
     override fun serialize(encoder: Encoder, value: UUID) {
         encoder.encodeString(value.toString())
+    }
+}
+
+object ColorSerializer : KSerializer<ChatColor> {
+    override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): ChatColor {
+        val text = decoder.decodeString()
+
+        return COLOR_ID_MAP[text] ?: ChatColor(
+            ' ', "", "", Color.decode(text).rgb
+        )
+    }
+
+    override fun serialize(encoder: Encoder, value: ChatColor) {
+        encoder.encodeString(
+            if (value.vanilla) {
+                value.id
+            } else {
+                Integer.toHexString(value.rgb)
+            }
+        )
     }
 }
